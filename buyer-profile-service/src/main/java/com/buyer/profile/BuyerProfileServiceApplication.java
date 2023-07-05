ca7083059b9a9353a32562489ac4c5f321ef2693
atiwat <boonsinatiwat2536@gmail.com>
2023-07-05 22:54:48 +0700

    add first commit for buyer profile service

---
+++ buyer-profile-service/src/main/java/com/buyer/profile/BuyerProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buyer.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyerProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyerProfileServiceApplication.class, args);
+	}
+
+}
